3ee8ab628cc49b1fdddf665d974f787c14c18797
diedona <diedona@gmail.com>
2014-03-28 22:16:25 -0300

    LISTAGEM DE DADOS EM UTC

--- Dona.DateTimeUtc/Program.cs
+++ Dona.DateTimeUtc/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dona.DateTimeUtc.db;
+using Dona.DateTimeUtc.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,32 @@ namespace Dona.DateTimeUtc
 {
     class Program
     {
+        private static FakeDb _db = new FakeDb();
+        private static TimeZoneInfo UserTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+
         static void Main(string[] args)
         {
+            Console.WriteLine("**** DATAS EM UTC/GMT");
+            ICollection<Pessoa> PessoasUTC = _db.Pessoas;
+            EscreverPessoas(PessoasUTC);
+
+            Console.WriteLine("**** DATAS TRADUZIDAS PARA '{0}'", UserTimeZoneInfo.Id);
+        }
+
+        private static void EscreverPessoas(ICollection<Pessoa> Pessoas)
+        {
+            foreach (var Pessoa in Pessoas)
+            {
+                Console.Write("Id: ");
+                Console.WriteLine(Pessoa.Id);
+                Console.Write("Nome: ");
+                Console.WriteLine(Pessoa.Nome);
+                Console.Write("Data de Aniversário: ");
+                Console.WriteLine(Pessoa.DtAniversario);
+                Console.Write("Data de Registro: ");
+                Console.WriteLine(Pessoa.DtCriacao);
+                Console.WriteLine();
+            }
         }
     }
 }
--- Dona.DateTimeUtc/db/FakeDb.cs
+++ Dona.DateTimeUtc/db/FakeDb.cs
@@ -19,19 +19,19 @@ namespace Dona.DateTimeUtc.db
                         DtAniversario = new DateTime(1989, 2, 15, 0, 0, 0, DateTimeKind.Utc),
                         DtCriacao = new DateTime(2012, 5, 5, 18, 0, 0, DateTimeKind.Utc), IsActive = true
                     },
-                    new Pessoa { Id = 1, Nome = "Manuel", 
+                    new Pessoa { Id = 2, Nome = "Manuel", 
                         DtAniversario = new DateTime(1986, 10, 5, 0, 0, 0, DateTimeKind.Utc),
                         DtCriacao = new DateTime(2013, 2, 1, 20, 0, 0, DateTimeKind.Utc), IsActive = true
                     },
-                    new Pessoa { Id = 1, Nome = "Carla", 
+                    new Pessoa { Id = 3, Nome = "Carla", 
                         DtAniversario = new DateTime(1997, 5, 11, 0, 0, 0, DateTimeKind.Utc),
                         DtCriacao = new DateTime(2013, 2, 12, 18, 0, 0, DateTimeKind.Utc), IsActive = true
                     },
-                    new Pessoa { Id = 1, Nome = "Kimoto", 
+                    new Pessoa { Id = 4, Nome = "Kimoto", 
                         DtAniversario = new DateTime(2000, 6, 12, 0, 0, 0, DateTimeKind.Utc),
                         DtCriacao = new DateTime(2014, 1, 28, 15, 0, 0, DateTimeKind.Utc), IsActive = true
                     },
-                    new Pessoa { Id = 1, Nome = "Ryu", 
+                    new Pessoa { Id = 5, Nome = "Ryu", 
                         DtAniversario = new DateTime(1975, 5, 6, 0, 0, 0, DateTimeKind.Utc),
                         DtCriacao = new DateTime(2012, 2, 1, 10, 0, 0, DateTimeKind.Utc), IsActive = true
                     }
