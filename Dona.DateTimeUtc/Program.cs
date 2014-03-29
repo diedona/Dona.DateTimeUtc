4b1227eef6c71c50aa0ed22e563b2753769ce3b0
diedona <diedona@gmail.com>
2014-03-28 22:55:25 -0300

    convertendo UTC para south america

--- Dona.DateTimeUtc/Program.cs
+++ Dona.DateTimeUtc/Program.cs
@@ -3,6 +3,7 @@ using Dona.DateTimeUtc.model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,10 +17,14 @@ namespace Dona.DateTimeUtc
         static void Main(string[] args)
         {
             Console.WriteLine("**** DATAS EM UTC/GMT");
+
             ICollection<Pessoa> PessoasUTC = _db.Pessoas;
             EscreverPessoas(PessoasUTC);
 
             Console.WriteLine("**** DATAS TRADUZIDAS PARA '{0}'", UserTimeZoneInfo.Id);
+
+            ICollection<Pessoa> PessoasConvertidas = ConverterDeUTC(PessoasUTC);
+            EscreverPessoas(PessoasConvertidas);
         }
 
         private static void EscreverPessoas(ICollection<Pessoa> Pessoas)
@@ -35,6 +40,68 @@ namespace Dona.DateTimeUtc
                 Console.Write("Data de Registro: ");
                 Console.WriteLine(Pessoa.DtCriacao);
                 Console.WriteLine();
+            }
+        }
+
+        private static ICollection<T> ConverterDeUTC<T>(ICollection<T> ListaOriginal) where T: class, new()
+        {
+            ICollection<T> ListaNova = new List<T>();
+
+            foreach (T Item in ListaOriginal)
+            {
+                T ItemNovo = ConverterDeUTC(Item);
+                ListaNova.Add(ItemNovo);
+            }
+
+            return ListaNova;
+        }
+
+        private static T ConverterDeUTC<T>(T Item) where T: class, new()
+        {
+            T Newitem = new T();
+
+            //todas as propriedades
+            PropertyInfo[] Properties = typeof(T).GetProperties();
+
+            //todas as que são data
+            PropertyInfo[] DateProperties = Properties.Where(x => x.PropertyType == typeof(DateTime) || x.PropertyType == typeof(DateTime?)).ToArray();
+
+            //todas as outras
+            PropertyInfo[] OtherProperties = Properties.Except(DateProperties).ToArray();
+
+            //convertendo as de data para a zona do usuário
+            foreach (var DateProperty in DateProperties)
+            {
+                DateTime UTCDate = Convert.ToDateTime(DateProperty.GetValue(Item));
+                DateTime UserTimeZoneDate = ConverterDeUTC(UTCDate);
+
+                DateProperty.SetValue(Newitem, UserTimeZoneDate);
+            }
+
+            //preenchendo os outros dados para o objeto
+            foreach (var OtherProperty in OtherProperties)
+            {
+                OtherProperty.SetValue(Newitem, OtherProperty.GetValue(Item));
+            }
+
+            return Newitem;
+        }
+
+        private static DateTime ConverterDeUTC(DateTime? ParamDate)
+        {
+            if (ParamDate.HasValue) return ConverterDeUTC(ParamDate.Value);
+            else return new DateTime();
+        }
+
+        private static DateTime ConverterDeUTC(DateTime ParamDate)
+        {
+            if(ParamDate.Kind == DateTimeKind.Utc)
+            {
+                return TimeZoneInfo.ConvertTime(ParamDate, TimeZoneInfo.Utc, UserTimeZoneInfo);
+            }
+            else
+            {
+                return new DateTime();
             }
         }
     }
--- Dona.DateTimeUtc/db/FakeDb.cs
+++ Dona.DateTimeUtc/db/FakeDb.cs
@@ -23,18 +23,18 @@ namespace Dona.DateTimeUtc.db
                         DtAniversario = new DateTime(1986, 10, 5, 0, 0, 0, DateTimeKind.Utc),
                         DtCriacao = new DateTime(2013, 2, 1, 20, 0, 0, DateTimeKind.Utc), IsActive = true
                     },
-                    new Pessoa { Id = 3, Nome = "Carla", 
-                        DtAniversario = new DateTime(1997, 5, 11, 0, 0, 0, DateTimeKind.Utc),
-                        DtCriacao = new DateTime(2013, 2, 12, 18, 0, 0, DateTimeKind.Utc), IsActive = true
-                    },
-                    new Pessoa { Id = 4, Nome = "Kimoto", 
-                        DtAniversario = new DateTime(2000, 6, 12, 0, 0, 0, DateTimeKind.Utc),
-                        DtCriacao = new DateTime(2014, 1, 28, 15, 0, 0, DateTimeKind.Utc), IsActive = true
-                    },
-                    new Pessoa { Id = 5, Nome = "Ryu", 
-                        DtAniversario = new DateTime(1975, 5, 6, 0, 0, 0, DateTimeKind.Utc),
-                        DtCriacao = new DateTime(2012, 2, 1, 10, 0, 0, DateTimeKind.Utc), IsActive = true
-                    }
+                    //new Pessoa { Id = 3, Nome = "Carla", 
+                    //    DtAniversario = new DateTime(1997, 5, 11, 0, 0, 0, DateTimeKind.Utc),
+                    //    DtCriacao = new DateTime(2013, 2, 12, 18, 0, 0, DateTimeKind.Utc), IsActive = true
+                    //},
+                    //new Pessoa { Id = 4, Nome = "Kimoto", 
+                    //    DtAniversario = new DateTime(2000, 6, 12, 0, 0, 0, DateTimeKind.Utc),
+                    //    DtCriacao = new DateTime(2014, 1, 28, 15, 0, 0, DateTimeKind.Utc), IsActive = true
+                    //},
+                    //new Pessoa { Id = 5, Nome = "Ryu", 
+                    //    DtAniversario = new DateTime(1975, 5, 6, 0, 0, 0, DateTimeKind.Utc),
+                    //    DtCriacao = new DateTime(2012, 2, 1, 10, 0, 0, DateTimeKind.Utc), IsActive = true
+                    //}
                 };
             }
         }
