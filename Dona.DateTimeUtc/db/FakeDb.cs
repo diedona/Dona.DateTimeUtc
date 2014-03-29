c7694c2926ed44a706ca87bc2763483a79aead8a
diedona <diedona@gmail.com>
2014-03-28 22:07:42 -0300

    Itens fake

---
+++ Dona.DateTimeUtc/db/FakeDb.cs
@@ -0,0 +1,42 @@
+﻿using Dona.DateTimeUtc.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dona.DateTimeUtc.db
+{
+    public class FakeDb
+    {
+        public ICollection<Pessoa> Pessoas 
+        { 
+            get
+            {
+                return new List<Pessoa>
+                {
+                    new Pessoa { Id = 1, Nome = "José", 
+                        DtAniversario = new DateTime(1989, 2, 15, 0, 0, 0, DateTimeKind.Utc),
+                        DtCriacao = new DateTime(2012, 5, 5, 18, 0, 0, DateTimeKind.Utc), IsActive = true
+                    },
+                    new Pessoa { Id = 1, Nome = "Manuel", 
+                        DtAniversario = new DateTime(1986, 10, 5, 0, 0, 0, DateTimeKind.Utc),
+                        DtCriacao = new DateTime(2013, 2, 1, 20, 0, 0, DateTimeKind.Utc), IsActive = true
+                    },
+                    new Pessoa { Id = 1, Nome = "Carla", 
+                        DtAniversario = new DateTime(1997, 5, 11, 0, 0, 0, DateTimeKind.Utc),
+                        DtCriacao = new DateTime(2013, 2, 12, 18, 0, 0, DateTimeKind.Utc), IsActive = true
+                    },
+                    new Pessoa { Id = 1, Nome = "Kimoto", 
+                        DtAniversario = new DateTime(2000, 6, 12, 0, 0, 0, DateTimeKind.Utc),
+                        DtCriacao = new DateTime(2014, 1, 28, 15, 0, 0, DateTimeKind.Utc), IsActive = true
+                    },
+                    new Pessoa { Id = 1, Nome = "Ryu", 
+                        DtAniversario = new DateTime(1975, 5, 6, 0, 0, 0, DateTimeKind.Utc),
+                        DtCriacao = new DateTime(2012, 2, 1, 10, 0, 0, DateTimeKind.Utc), IsActive = true
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ Dona.DateTimeUtc/model/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dona.DateTimeUtc.model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DtAniversario { get; set; }
+        public DateTime DtCriacao { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
